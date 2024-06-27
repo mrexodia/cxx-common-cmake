1761b35ee0ddf261bc69c896ab6662e381b01509
kyle-elliott <kyle_a_elliott@protonmail.ch>
2024-06-27 12:21:36 -0400

    Update to LLVM 17
    - Detect for clang on linux and add PIC flag to allow for linking into a shared library
    - Apply flags on xed during the case above

--- llvm.cmake
+++ llvm.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(llvm
     URL
-        "https://github.com/llvm/llvm-project/releases/download/llvmorg-15.0.4/llvm-project-15.0.4.src.tar.xz"
+        "https://github.com/llvm/llvm-project/releases/download/llvmorg-17.0.6/llvm-project-17.0.6.src.tar.xz"
     URL_HASH
-        "SHA256=a3112dca9bdea4095361829910b74fb6b9da8ae6e3500db67c43c540ad6072da"
+        "SHA256=58a8818c60e6627064f312dbf46c02d9949956558340938b71cf731ad8bc0813"
     CMAKE_CACHE_ARGS
         ${CMAKE_ARGS}
         "-DLLVM_ENABLE_PROJECTS:STRING=clang;lld"
